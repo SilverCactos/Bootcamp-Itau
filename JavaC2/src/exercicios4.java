6ec64de1ea21987c942c37550e229db5a62a812b
SilverCactos <srcactos@gmail.com>
2026-09-03 17:44:35 -0300

    Exercício 4

--- JavaC2/src/exercicios4.java
+++ JavaC2/src/exercicios4.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class exercicios4 {
-    
+    public static void main(String[] args) throws Exception {
+        
+        var scanner = new Scanner(System.in);
+
+        System.out.println("Insira o primeiro número: ");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Insira o segundo número: ");
+        int num2 =  scanner.nextInt();
+
+        if (num2 > num1) {
+                System.out.println("O primeiro número inserido não pode ser menor que o segundo!");
+        }
+
+        while (num1 % num2 == 0) {
+            System.out.println("Insira outro número: ");
+            num2 = scanner.nextInt();
+        }
+    }
 }
